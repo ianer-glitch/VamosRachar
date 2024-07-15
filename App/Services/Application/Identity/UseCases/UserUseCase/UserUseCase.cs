94a80ff951ac0e2278373b2d915bb5a5862678fa
ianer-glitch <ianrepker@gmail.com>
2024-07-15 18:48:01 -0300

    creating useCaseUser with grpc

---
+++ App/Services/Application/Identity/UseCases/UserUseCase/UserUseCase.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Identity.UseCases.UserUseCase
+{
+    public class UserUseCase : payloadExampleSerivce.payloadExampleSerivceBase
+    {
+        public override async Task<PayloadMessage> GetPayloadMessage(PayloadMessage request, ServerCallContext context)
+        {
+            return await Task.FromResult(request);
+        }
+    }
+}
